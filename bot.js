24f97d889fe8d10510b953ca1641ea70ca307524
Randy Apuzzo <apuzzo@gmail.com>
2017-11-07 13:55:26 -0800

    configured to run in chan

--- bot.js
+++ bot.js
@@ -1,7 +1,9 @@
-var Discord = require('discord.io');
+require('dotenv').config()
 
+var Discord = require('discord.io');
+var request = require('request');
 var logger = require('winston');
-var auth = require('./auth.json');
+
 
 
 // Configure logger settings
@@ -11,10 +13,11 @@ logger.add(logger.transports.Console, {
 });
 logger.level = 'debug';
 
+console.log(process.env)
 
 // Initialize Discord Bot
 var bot = new Discord.Client({
-    token: auth.token,
+    token: process.env.USERTOKEN,
     autorun: true
 });
 
@@ -30,14 +33,41 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     // for this script it will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
+
+        console.log(args)
+
         var cmd = args[0];
 
         args = args.splice(1);
 
         switch(cmd) {
             // !ping
-            case 'ping':
-                bot.sendMessage({ to: channelID, message: 'Pong!' });
+            case 'card':
+                var cardName =  args.join(' ')
+                //console.log(cardName)
+                // Configure the request
+                var options = {
+                    url: 'http://dev.echomtg.com/api/discord/card/',
+                    method: 'POST',
+                    form: {'token': process.env.ECHOKEY, 'text': cardName}
+                }
+
+                //console.log(options)
+
+                // Start the request
+                request(options, function (error, response, body) {
+                    console.log('in teh request '+ response.statusCode)
+                    if (!error && response.statusCode == 200) {
+                        // Print out the response body
+                        //console.log(body)
+
+                        bot.sendMessage({ to: channelID, message: body });
+                    } else {
+                        bot.sendMessage({ to: channelID, message: 'Error: '+error });
+
+                    }
+
+                })
             break;
             default:
                 bot.sendMessage({ to: channelID, message: 'Unknown command.' });
